8b57d45f525d8a4bc9e5fd45d4b7b9e2bd0085f4
29534411212 <tkalyoncuoglu@gmail.com>
2024-12-28 20:42:50 +0300

    refactored

--- CSharp/PayRoll/Employee.cs
+++ CSharp/PayRoll/Employee.cs
@@ -26,6 +26,9 @@ namespace PayRoll
 
         public decimal CalculatePayAmount()
         {
+            if (Data == null && PayCheckBehavior.IsT0)
+                throw new InvalidOperationException("EmployedData is required but is null.");
+
             return PayCheckBehavior.Match(f => f(Data), f => f());
         }
         
